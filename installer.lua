26f21e5d02033b773c3ae720e729d0704c637123
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-08-28 11:30:19 +0200

    Simplifying file and adding some time before reboot

--- installer.lua
+++ installer.lua
@@ -3,13 +3,12 @@ if switchcraft then
   shell.run("gitget","knijn","osmium","main")
   fs.delete("installer.lua")
   fs.delete("README.md")
+  print("Installed Osmium, rebooting now")
 else
-  print("Geting file 1/3")
   shell.run("wget https://github.com/knijn/osmium/blob/main/os.lua os.lua")
-  print("Geting file 2/3")
   shell.run("wget https://github.com/knijn/osmium/blob/main/home.lua home.lua")
-  print("Geting file 3/3")
   shell.run("wget https://github.com/knijn/osmium/blob/main/startup.lua startup.lua")
-  print("Installed.")
+  print("Installed Osmium, rebooting now")
 end
+sleep(1)
 os.reboot()
