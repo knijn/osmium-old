37526b36af5fcf717c1d2ef9aa290fd443224c36
RubenHetKonijn <64010592+RubenHetKonijn@users.noreply.github.com>
2021-08-28 11:24:23 +0200

    New home screen

--- osmium/home.lua
+++ osmium/home.lua
@@ -1,20 +1,91 @@
-local backgroundColor = colors.black
-local overlayColor = colors.gray
-local accentColor = colors.cyan
-local textColor = colors.white
+-- Libraries
+local common = require("/osmium/libs/common")
+local CommonUI = require("/osmium/libs/commonui").new()
 
-term.setBackgroundColor(backgroundColor)
-term.setTextColor(textColor)
-term.setCursorPos(1,2)
+-- Config setup
+local confighandle = fs.open("/osmium/config.lua","r")
+local config = textutils.unserialiseJSON(confighandle.readAll())
+confighandle.close()
 
-local files = fs.list("/")
-for i,o in pairs(files) do
-  if fs.isDir(o) then
-    local oldColor = term.getTextColor()
-    term.setTextColor(accentColor)
-    print(i .. " - " .. o)
-    term.setTextColor(oldColor)
-    return
-  end
-  print(i .. " - " .. o)
+
+-- Color variable setup
+local backgroundColor = common.returnColor(config.backgroundColor)
+local textColor = common.returnColor(config.textColor)
+local accentColor = common.returnColor(config.accentColor)
+local tertiaryColor = common.returnColor(config.tertiaryColor)
+local tertiaryTextColor = common.returnColor(config.tertiaryTextColor)
+
+CommonUI.ClearScreen(backgroundColor)
+
+local appWindow = window.create(term.current(),1,5,20,10)
+
+local drawMenu = function ()
+  CommonUI:AddAndDrawComponent(
+    CommonUI.NewButton(
+        "Shut Down",
+        2,4,
+        false,
+        tertiaryColor,
+        tertiaryTextColor,
+        function()
+            os.shutdown()
+        end
+    )
+  )
+  CommonUI:AddAndDrawComponent(
+    CommonUI.NewButton(
+        "Exit to shell",
+        2,5,
+        false,
+        tertiaryColor,
+        tertiaryTextColor,
+        function()
+            term.setBackgroundColor(colors.black)
+            term.setTextColor(colors.white)
+            term.clear()
+            term.setCursorPos(1,1)
+            error("Exited to shell, Goodbye!",0)
+        end
+    )
+  )
 end
+local drawApps = function ()
+  local oldTerm = term.current()
+  term.redirect(appWindow)
+  CommonUI.ClearScreen(backgroundColor)
+  for i,o in pairs(fs.list("/apps/")) do
+    local cleanName = o:gsub("%.lua", "")
+    CommonUI:AddAndDrawComponent(
+      CommonUI.NewButton(
+        cleanName,  -- Button text
+        1, 1+i,         -- x and y position
+        false,         -- is the button a big or a small one?
+        accentColor, -- background color
+        textColor, -- foreground (text) color
+        function()    -- gets called when the button is clicked
+          --CommonUI.ClearScreen(colors.black)
+          shell.openTab("/apps/" .. o)
+        end))
+  end
+  term.redirect(oldTerm)
+end
+
+
+CommonUI.DrawBox(1,1,26,2,accentColor)
+
+CommonUI:AddAndDrawComponent(
+    CommonUI.NewButton(
+        "O",
+        1,1,
+        true,
+        accentColor,
+        textColor,
+        function()
+            drawMenu()
+        end
+    )
+)
+
+drawApps()
+
+CommonUI:BeginEventLoop()
